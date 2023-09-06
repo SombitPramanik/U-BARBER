73a039d29d82a3b00e58a2e17d0bcf23acfcbd29
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-06 21:56:47 +0530

    update the file co-ordination && fix session tocken

---
+++ register.php
@@ -0,0 +1,84 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require './config.php';
+session_unset();
+session_destroy();
+
+if (isset($_POST["submit"])) {
+    $first_name = $_POST["f_name"];
+    $last_name = $_POST["l_name"];
+    $email = $_POST["email"];
+    $mobile = $_POST["m_num"];
+    $password = $_POST["password"];
+    $c_password = $_POST["c_password"];
+    $duplicate = mysqli_query($conn, "SELECT * FROM normal_user Where email = '$email'");
+
+
+    if (mysqli_num_rows($duplicate) > 0) {
+        echo "<script>alert('Email and User Name is Already Taken\nUse Another One');</script>";
+    } else {
+        if ($password == $c_password) {
+            echo "<script>alert('Registration successful\n ! your username is $email\nRedirecting in 5 seconds');</script>";
+            $query = "INSERT INTO normal_user VALUES('','$first_name','$last_name','$email','$password','$mobile')";
+            mysqli_query($conn, $query);
+            // echo"<script>alert('Registration successful\n ! your username is $email\nRedirecting in 10 seconds');</script>";
+            echo "<script>setTimeout(function() { window.location.href = 'index.php'; }, 5000);</script>";
+            exit();
+        } else {
+            echo "<script>alert('Password Does Not Match [ $password Not Qual to $c_password ] Please Try again');</script>";
+        }
+    }
+    // echo "<script>setTimeout(function() { window.location.href = 'index.php'; }, 5000);</script>";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="language" content="en">
+    <meta name="description" content="Discover reliable server and hosting solutions tailored to your needs at [Your Website Name]. We specialize in delivering cutting-edge hosting services, including shared, VPS, and dedicated hosting, ensuring your website's optimal performance. Take advantage of our expertise in custom server configurations, allowing you to build a server environment that perfectly matches your business requirements. Additionally, we offer advanced storage solutions, empowering you with scalable and secure data management. Explore our range of cost-effective plans and enhance your online presence with seamless server solutions">
+    <meta name="keywords" content="web services, amazon web services, server,sql server,free website hosting, free domain and hosting, hosting, development server, sombit web services,https://hosting.sombti-server.online,">
+    <meta name="author" content="Sombit Pramanik">
+    <meta property="og:title" content="Sombit Web Services">
+    <meta property="og:description" content="Discover reliable server and hosting solutions tailored to your needs at Sombit Web Services. We specialize in delivering cutting-edge hosting services, including shared, VPS, and dedicated hosting, ensuring your website's optimal performance. Take advantage of our expertise in custom server configurations, allowing you to build a server environment that perfectly matches your business requirements. Additionally, we offer advanced storage solutions, empowering you with scalable and secure data management. Explore our range of cost-effective plans and enhance your online presence with seamless server solutions.">
+    <meta property="og:image" content="/image/sws.png">
+    <meta property="og:url" content="https://hosting.sombti-server.online">
+    <meta property="og:type" content="website">
+    <link rel="icon" href="./U-BARBER.ico" type="image/x-icon">
+    <title>U-BARBER Registration</title>
+    <link rel="stylesheet" href="./register.css">
+    <link rel="stylesheet" href="./responsive.css">
+</head>
+
+<body>
+    <img src="./U-BARBER.png" alt="" srcset="">
+    <form action="" method="post" class="ref_form">
+        <label for="f_name">First Name </label>
+        <input type="text" name="f_name" id="f_name" required value="" placeholder="Enter Your First Name"><br>
+        <label for="l_name">Last Name </label>
+        <input type="text" id="l_name" name="l_name" required value="" placeholder="Enter Your Last Name"><br>
+        <label for="email">Email </label>
+        <input type="email" id="email" name="email" required value="" placeholder="Enter Your Email"><br>
+        <label for="m_num">Mobile Number </label>
+        <input type="tel" id="m_num" name="m_num" required value="" placeholder="Enter your mobile Number"><br>
+        <label for="password">Password </label>
+        <input type="password" id="password" name="password" required value="" placeholder="Enter Super Strong Password"><br>
+        <label for="c_password">Confirm Password</label>
+        <input type="password" id="c_password" name="c_password" required value="" placeholder="Re-Enter Same Super Strong Password"><br>
+        <button type="submit" name="submit" id="submit"> Register !</button>
+    </form>
+    <br>
+    <a href="./index.php">Log In</a>
+
+
+
+</body>
+
+</html>
--- user.php
+++ user.php
@@ -3,29 +3,23 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require './config.php';
+require 'config.php';
 
-if (isset($_POST["submit"])) {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+
+if (!empty($_SESSION["session_token"])) {
+    $session_token = $_SESSION["session_token"];
+    $email = $session_token;
 
     $result = mysqli_query($conn, "SELECT * FROM normal_user WHERE email = '$email'");
     $row = mysqli_fetch_assoc($result);
 
-    if (mysqli_num_rows($result) > 0) {
-        if ($password == $row["password"]) {
-            $session_token = $email; // Use email as session token
-            $_SESSION["login"] = true;
-            $_SESSION["session_token"] = $session_token;
-            header("location: user.php");
-            exit();
-        } else {
-            echo "<script>alert('Wrong Password');</script>";
-        }
-    } else {
-        echo "<script>alert('Username or email is not found :(  Register Now!');</script>";
+    if (!$row) {
+        header("location: index.php"); // Invalid session token, redirect to login
     }
+} else {
+    header("location: index.php"); // No session token, redirect to login
 }
+
 ?>
 
 <!DOCTYPE html>
@@ -43,35 +37,36 @@ if (isset($_POST["submit"])) {
     <meta property="og:image" content="/image/sws.png">
     <meta property="og:url" content="https://hosting.sombti-server.online">
     <meta property="og:type" content="website">
-    <link rel="icon" href="./IMG/U-BARBER.png" type="image/x-icon">
+    <link rel="icon" href="/image/sws.ico" type="image/x-icon">
     <title>Sombit Web Services</title>
     <!-- 
         Website External Page's and Style Link Section
      -->
-    <!-- <link rel="stylesheet" href="index.css"> -->
-    <link rel="stylesheet" href="./responsive.css">
+    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/responsive.css">
+    <link rel="stylesheet" href="/com.css">
+    
     <!-- 
         Internal CSS Goes down
-    -->
+      -->
 
     <style>
+
     </style>
 </head>
 
 <body>
-    <img src="./U-BARBER.png" alt="" srcset=""><br>
-    <form action="" method="post" class="login_form">
-        <label for="email">What is your Email </label>
-        <input type="email" placeholder="please tell me" name="email" id="email" required><br>
-        <label for="password">What is your password </label>
-        <input type="password" placeholder="Promise, I don't tell anyone" name="password" id="password" required> <br>
-        <button type="submit" name="submit" id="login_btn">Log In</button>
-    </form>
-    <a href="./register.php">Register Hear</a>
+    <main class="blur-background">
+        <span>our pluse point >>>></span>
+        <div id="typing-container"></div>
+        <section id="home">
 
-
-
-
+            <h1>Welcome <?php echo $row["f_name"]; ?> <?php echo $row["l_name"]; ?></h1><br>
+            <a href="/logout.php">Log out</a><br>
+            <a href="/registration.php">New Registration</a>
+            <div class="emoji">🚀</div>
+            
 </body>
 
 </html>
+<script src="script.js"></script>
