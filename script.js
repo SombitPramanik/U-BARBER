7341f76bd2311b96c431000b662881a882fb1d70
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-13 12:42:53 +0530

    fix Fetch Issue in script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ for (let i = 0; i < openButtons.length; i++) {
         const orderId = this.getAttribute("data-order-id"); // Get the unique ID
         // Send the orderId to your PHP script, e.g., using AJAX
         // You can replace the following code with your AJAX implementation
-        fetch('order.php', {
+        fetch('user.php', {
             method: 'POST',
             body: JSON.stringify({ orderId: orderId }),
             headers: {
