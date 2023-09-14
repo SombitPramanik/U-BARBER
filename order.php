727a02fb667fa58ce67b579b00429c4fa829d61d
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-14 13:28:16 +0530

    fix some issue and add CSS to order page

--- order.php
+++ order.php
@@ -21,10 +21,11 @@ if (isset($_POST["submit"])) {
     $name = $_POST["name"];
     $mobile = $_POST["mobile"];
     $order_id = $_POST["order_id"];
+    $price = $_POST["price"];
     $time = time();
 
 
-    $query = "INSERT INTO receive_order VALUES('$time','$name','$mobile','$order_id')";
+    $query = "INSERT INTO receive_order VALUES('$time','$name','$mobile','$order_id','$price')";
     mysqli_query($conn, $query);
 }
 
@@ -46,7 +47,70 @@ if (isset($_POST["submit"])) {
     <meta property="og:type" content="website">
     <link rel="icon" href="./U-BARBER.ico" type="image/x-icon">
     <title>Review Order / <?php echo ucwords($row["f_name"] . " " . $row["l_name"]); ?></title>
-    
+    <style>
+        body {
+            background: linear-gradient(to bottom, rgb(207, 235, 245), rgb(255, 255, 255));
+            margin: 0;
+            padding: 0;
+            height: 100vh;
+        }
+
+        .order_form {
+            margin: 5px;
+        }
+
+        input {
+            width: 90%;
+            font-weight: bold;
+            overflow: hidden;
+            border-radius: .3em;
+            margin: .5em;
+            height: 2em;
+            padding: .3em;
+            border: 4px solid yellow;
+        }
+
+        input::placeholder {
+            text-transform: capitalize;
+
+        }
+
+        label {
+            text-align: left !important;
+            margin-left: .5em;
+            margin-right: .5em;
+            font-size: large;
+            font-weight: bold;
+        }
+
+        button {
+            margin-top: 2em;
+            margin-left: 8.9vw;
+            background-color: #2ecc71;
+            font-size: large;
+            font-weight: bolder;
+            color: rgb(55, 54, 60);
+            border-radius: .9em;
+            border: 3px solid rgb(188, 195, 111);
+            padding: .5em;
+            width: 80%;
+            cursor: pointer;
+
+        }
+
+        a {
+            margin: auto;
+            color: rgb(49, 14, 175);
+            text-decoration: none;
+            font-size: large;
+            font-weight: bold;
+            border-radius: 2em;
+            border: 3px solid black;
+            padding: 5px 20px 5px 20px;
+            background-color: #35d47f;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -58,33 +122,38 @@ if (isset($_POST["submit"])) {
         <input type="tel" id="mobile" name="mobile" required value="<?php echo $row["mobile"]; ?>"><br>
         <label for="order_id">Order ID </label>
         <input type="text" id="order_id" name="order_id" contenteditable="false" readonly required><br>
+        <label for="price">Price </label>
+        <input type="text" id="price" name="price" contenteditable="false" readonly required><br>
 
         <button type="submit" name="submit" id="submit"> Register !</button>
     </form>
     <br>
 </body>
 <script>
-    // Function to update the orderId field based on local storage
     function updateOrderIdField() {
-        // Retrieve orderId from local storage
         const orderId = localStorage.getItem("orderId");
+        const price = localStorage.getItem("price");
 
-        // Check if orderId exists and if the element with id "order_id" exists
+
+        if (price && document.getElementById("price")) {
+            const current_price = document.getElementById("price").value;
+
+            if (price !== current_price) {
+                document.getElementById("price").innerText = price;
+                document.getElementById("price").value = price;
+            }
+        }
+
         if (orderId && document.getElementById("order_id")) {
-            // Get the current value of the order_id field
             const currentOrderId = document.getElementById("order_id").value;
 
-            // Check if the orderId has changed
             if (orderId !== currentOrderId) {
-                // Update the orderId field
                 document.getElementById("order_id").innerText = orderId;
                 document.getElementById("order_id").value = orderId;
             }
         }
     }
-
-    // Set an interval to continuously update the orderId field
-    const updateInterval = setInterval(updateOrderIdField, 100); // Update every 1 second (adjust as needed)
+    const updateInterval = setInterval(updateOrderIdField, 100);
 </script>
 
 </html>
