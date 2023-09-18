33bc3e7ad9755359f1756a54b7a2517feec319bb
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-09-18 07:50:17 +0530

    fix the direct acces issue with the session tocken

--- update_ctl.php
+++ update_ctl.php
@@ -1,7 +1,7 @@
 <?php
-if(!defined('allow')){
-    die('Direct Access Denied');
-}
+// if(!defined('allow')){
+//     die('Direct Access Denied');
+// }
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
